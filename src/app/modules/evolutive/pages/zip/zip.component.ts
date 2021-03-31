64bc38f456778fd80f19cf1e14131a7c9ca7b6bf
Luigi Minopoli <l.minopoli@software-inside.it>
2021-03-31 09:48:23 +0200

    Final checkbox - Zip

--- src/app/modules/evolutive/pages/zip/zip.component.ts
+++ src/app/modules/evolutive/pages/zip/zip.component.ts
@@ -12,30 +12,32 @@ export class ZipComponent {
     { name: "Pinguino", checked: false, folder: "penguino", elements: [{ file: "Penguin.jpg", path: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/01/16/10/emperor-penguin.jpg", }] },
     { name: "Bradipo", checked: false, folder: "sloth", elements: [{ file: "Didattilo.jpg", path: "http://mashable.com/wp-content/uploads/2013/07/two-toed-sloth.jpg", }, { file: "Bradipo.jpg", path: "https://d.newsweek.com/en/full/1537892/sloth.jpg", }] },
     { name: "Giraffa", checked: false, folder: "girafa", elements: [{ file: "Giraffe.jpg", path: "https://www.waveexpeditions.ug/wp-content/uploads/2019/11/giraffe-in-uganda.jpg", },] },
-    { name: "Auto", checked: false, folder: "supercar", elements: [{ file: "Lambo.jpg", path: "https://www.autotoday.it/wp-content/uploads/2019/05/HURACAN-EVO.jpg", }, {file: "Koenigsegg.jgp", path: "https://sm.askmen.com/t/askmen_me/article/2/2015-frank/2015-frankfurt-auto-show-reveals_j7g5.1200.jpg"}] },
+    { name: "Auto", checked: false, folder: "supercar", elements: [{ file: "Lambo.jpg", path: "https://www.autotoday.it/wp-content/uploads/2019/05/HURACAN-EVO.jpg", }, { file: "Koenigsegg.jpg", path: "https://sm.askmen.com/t/askmen_me/article/2/2015-frank/2015-frankfurt-auto-show-reveals_j7g5.1200.jpg" }] },
   ];
 
   public get IsEnabled(): boolean { return this.scelte.some(scelta => scelta.checked); }
 
-  constructor(private toolbarService: ToolbarService) {
+  public get AllChecked(): boolean { return this.scelte.every(scelta => scelta.checked); }
+  public get SomeChecked(): boolean { return this.scelte.some(scelta => scelta.checked) && this.scelte.some(scelta => !scelta.checked); }
+
+  public constructor(private toolbarService: ToolbarService) {
     this.toolbarService.title = "Zipper";
   }
 
-  download() {
+  public selectAll() {
+    const checked = this.AllChecked;
+    this.scelte.forEach(scelta => scelta.checked = !checked);
+  }
+
+  public download() {
     const zip: JSZip = new JSZip();
 
-    const folders = [...new Set(this.scelte.filter(scelta => scelta.checked).map(scelta => scelta.folder))];
-
-    folders.forEach(folder => {
-      const currentFolder = zip.folder(folder);
-
-      this.scelte
-        .filter(scelta => scelta.folder == folder)
-        .forEach(file => file.elements
-          .forEach(element => currentFolder?.file(element.file, element.path))
-        )
-        ;
-    });
+    this.scelte
+      .filter(scelta => scelta.checked)
+      .forEach(scelta => {
+        const currentFolder = zip.folder(scelta.folder);
+        scelta.elements.forEach(element => currentFolder?.file(element.file, element.path));
+      });
 
     zip.generateAsync({ type: "blob" }).then(content => {
       let link = document.createElement('a');
